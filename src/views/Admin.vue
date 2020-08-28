66adaa69a448a996a5685624eeedc6d0a27da14d
M3ANMAXINE <CHERRERAJ@HOTMAIL.COM>
2020-08-27 22:21:15 -0400

    autentificaciones con firebase

---
+++ src/views/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>GRACIAS POR LA AYUDA</h1>
+    <h2>BIENVENIDO {{ currentUser.email }} </h2>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['currentUser'])
+  }
+
+}
+</script>
+
+<style>
+
+</style>
